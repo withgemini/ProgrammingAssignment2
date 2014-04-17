aad6e0cacf2ddc6b9ae3867dd2b35ad26b613642
withgemini <withgemini@yahoo.com>
2014-04-17 10:48:45 -0700

    completed assignment
    wrote 2 functions, makeCacheMatrix and cacheSolve. For inverse
    calculation, could have used corpcor package instead of solve(), but
    the assignment seems to indicate that solve() is sufficient.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve together enable a faster access of matrix inverse after
+## the initial calculation, through "caching" the inverse of the given matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a "special" matrix and returns a list of 4 functions 
+## that enable more efficient inverse access using cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse 
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)   
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates inverse for a "special" matrix, created using makeCacheMatrix. 
+## This function stores the inverse and returns it, when the function is called on the matrix 
+## for the first time. When the function is called on the "special" matrix,for which the inverse 
+## has been already calculated, it accesses its stored value without any recalculations. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...) #alternatively, could have used corpcor package
+    x$setinverse(i)
+    i
 }
